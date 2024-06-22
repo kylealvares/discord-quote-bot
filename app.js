d984810bbec0413424dbff03d659f30565417000
kylealvares <47119553+kylealvares@users.noreply.github.com>
2024-06-21 20:25:25 -0400

    ♻️ Refactor app from server → script

--- app.js
+++ app.js
@@ -1,63 +1,62 @@
-import axios from "axios";
+import axios from 'axios';
 import dotenv from 'dotenv';
-import express from 'express';
 import { Client, Events, GatewayIntentBits } from 'discord.js';
-import { formatQuote } from "./src/utils.js";
+import { formatQuote } from './src/utils.js';
 dotenv.config();
 
-const port = process.env.PORT || 3000;
+async function invokeQuoteOfTheDay() {
+  const client = new Client({
+    intents: [
+      GatewayIntentBits.Guilds,
+      GatewayIntentBits.GuildMessages,
+    ],
+  });
 
-const app = express();
+  client.once(Events.ClientReady, async () => {
+    console.log('✅ Bot is ready');
+    const quotesChannel = client.channels.cache.get(process.env.DEV_CHANNEL_ID);
+    if (!quotesChannel) {
+      console.error('\n❌ Error! Failed to find quotes channel.');
+      await client.destroy();
+      process.exit(1);
+    }
 
-app.get("/", (_, res) => {
-    res.send('🤖 Discord Quotes Bot is running...');
-});
+    console.log('✅ Quotes channel found');
 
-app.get("/quote-of-the-day", (_, res) => {
+    let quote = null;
 
-    const client = new Client({
-        intents: [
-            GatewayIntentBits.Guilds,
-            GatewayIntentBits.GuildMessages,
-        ],
-    });
+    try {
+      const response = await axios.get('https://zenquotes.io/api/today');
+      console.log('💬 Quote API response:', response.data); // Log the response to see its structure
+      quote = response; // Pass the entire response to formatQuote
+      console.log('ℹ️ Formatted quote:', formatQuote(quote)); // Log the formatted quote
+    } catch (err) {
+      console.error('\n❌ Error! Failed to fetch the quote of the day.\n', err);
+      await client.destroy();
+      process.exit(1);
+    }
 
-    client.login(process.env.DISCORD_TOKEN);
+    console.log('✅ Quote fetched');
 
-    client.once(Events.ClientReady, async (c) => {
+    try {
+      await quotesChannel.send(formatQuote(quote));
+      console.log('✅ Success! Message sent to quotes channel!');
+      await client.destroy();
+      process.exit(0);
+    } catch (err) {
+      console.error('\n❌ Error! Failed to send message in quotes channel.\n', err);
+      await client.destroy();
+      process.exit(1);
+    }
+  });
 
-        // Cache quotes channel
-        const quotesChannel = client.channels.cache.get(process.env.QUOTES_CHANNEL_ID);
-        if (!quotesChannel) {
-            console.error('\n❌ - Error! Failed to find quotes channel.');
-            return res.status(500).send('❌ - Error! Failed to find quotes channel.');
-        }
+  try {
+    await client.login(process.env.DISCORD_TOKEN);
+    console.log('✅ Bot logged in');
+  } catch (err) {
+    console.error('\n❌ Error! Failed to login to Discord.\n', err);
+    process.exit(1);
+  }
+}
 
-        let quote = null;
-
-        // Fetch quote from zenquotes
-        try {
-            quote = await axios.get("https://zenquotes.io/api/today");
-        } catch (err) {
-            console.error('\n❌ - Error! Failed to fetch the quote of the day.\n');
-            console.error(err);
-            return res.status(500).send('❌ - Error! Failed to fetch the quote of the day.\n' + err);
-        }
-
-        // Send formatted quote in quotes channel
-        try {
-            quotesChannel.send(formatQuote(quote));
-            console.log('✅ - Success! Message sent to quotes channel!');
-            return res.status(200).send('✅ - Success! Message sent to quotes channel!');
-        } catch (err) {
-            console.error('\n❌ - Error! Failed to send message in quotes channel.\n');
-            console.error(err);
-            return res.status(500).send('❌ - Error! Failed to send message in quotes channel.\n' + err);
-        }
-    });
-
-});
-
-app.listen(port, () => {
-    console.log('🤖 Discord Quotes Bot is running on port', port, '.');
-});
+invokeQuoteOfTheDay();
